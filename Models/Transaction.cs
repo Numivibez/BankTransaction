09cf1c9a2bfeb267719f99bbc7bb9bc70b7f9f71
James <james.babalola0@gmail.com>
2026-06-16 15:35:38 +0100

    Add project files.

---
+++ Models/Transaction.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BankTransaction.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int TransactionId { get; set; }
+
+        [Column(TypeName = "nvarchar(12)")]
+        [DisplayName("Account Number")]
+        [Required(ErrorMessage = "Account Number is required")]
+        [MaxLength(12, ErrorMessage = "Account Number must be 12 characters or less")]
+        public string AccountNumber { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        [DisplayName("Beneficiary Name")]
+        [Required(ErrorMessage = "Beneficiary Name is required")]
+        public string BeneficiaryName { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        [DisplayName("Bank Name")]
+        [Required(ErrorMessage = "Bank Name is required")]
+        public string BankName { get; set; }
+
+        [Column(TypeName = "nvarchar(11)")]
+        [DisplayName("SWIFT Code")]
+        [Required(ErrorMessage = "SWIFT Code is required")]
+        [MaxLength(11, ErrorMessage = "Swift Code must be 12 characters or less")]
+        public string SwiftCode { get; set; }
+
+        [Required(ErrorMessage = "Amount is required")]
+        public int Amount { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:MMM-dd-yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime Date { get; set; }
+    }
+}
